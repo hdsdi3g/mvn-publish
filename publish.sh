f24212ef751c7b53114bf310292083c76433c79c
hdsdi3g <admin@hd3g.tv>
2024-04-10 15:15:33 +0200

    Add a publish option to call a linux-springboot-package #4

--- publish.sh
+++ publish.sh
@@ -64,35 +64,41 @@ fi
 
 if [[ "$NEW_POM_VERSION" =~ .*"SNAPSHOT".*  ]]; then
 	cmd=(whiptail --title "Confirm actions" --checklist "What do you want to do with new $NEW_POM_VERSION?" 0 0 4);
-	cmd+=("4" "Clean and test" OFF);
-	cmd+=("6" "Install locally" OFF);
-	cmd+=("9" "Create git branch" OFF);
-	cmd+=("5" "Commit new pom.xml (and auto-generated files)" ON);
-	cmd+=("7" "Git push" ON);
+	cmd+=("A04" "Clean and test" OFF);
+	cmd+=("A06" "Install locally" OFF);
+	cmd+=("A09" "Create git branch" OFF);
+	cmd+=("A05" "Commit new pom.xml (and auto-generated files)" ON);
+	cmd+=("A07" "Git push" ON);
 	ACTION_LIST=$("${cmd[@]}" 3>&1 1>&2 2>&3);
 else
 	cmd=(whiptail --title "Confirm actions" --checklist "What do you want to do with current $NEW_POM_VERSION?" 0 0 4);
-	cmd+=("4" "Clean and test" OFF);
-	cmd+=("6" "Install locally" OFF);
+	cmd+=("A04" "Clean and test" OFF);
+	cmd+=("A06" "Install locally" OFF);
 	if [[ "$BASE_BRANCH" != "" && "$ACTUAl_BRANCH" != "master" && "$ACTUAl_BRANCH" != "main" ]]; then
-		cmd+=("12" "Verify current PR status" ON);
+		cmd+=("A12" "Verify current PR status" ON);
 	fi
-	cmd+=("1c" "Clean, deploy" ON);
-	cmd+=("1d" "Clean, test and deploy" OFF);
-	cmd+=("1a" "Staging release" OFF);
+	cmd+=("A1C" "Clean, deploy" ON);
+	cmd+=("A1D" "Clean, test and deploy" OFF);
+	cmd+=("A1A" "Staging release" OFF);
 	if [ -f "$CHANGELOGFILE" ]; then
-		cmd+=("14" "Edit $CHANGELOGFILE" ON);
+		cmd+=("A14" "Edit $CHANGELOGFILE" ON);
 	fi
-	cmd+=("2" "Commit new pom.xml (and auto-generated files)" ON);
-	cmd+=("3" "Git tag to $NEW_POM_VERSION" ON);
-	cmd+=("7" "Git push $ACTUAl_BRANCH" ON);
-	cmd+=("8" "Git push tag" ON);
+	cmd+=("A02" "Commit new pom.xml (and auto-generated files)" ON);
+	cmd+=("A03" "Git tag to $NEW_POM_VERSION" ON);
+	cmd+=("A07" "Git push $ACTUAl_BRANCH" ON);
+	cmd+=("A08" "Git push tag" ON);
 	if [[ "$BASE_BRANCH" != "" && "$ACTUAl_BRANCH" != "master" && "$ACTUAl_BRANCH" != "main"  ]]; then
-		cmd+=("10" "Git merge $ACTUAl_BRANCH to $BASE_BRANCH" ON);
-		cmd+=("11" "Git delete local $ACTUAl_BRANCH" ON);
-		cmd+=("13" "Git (final) push to $BASE_BRANCH" ON);
+		cmd+=("A10" "Git merge $ACTUAl_BRANCH to $BASE_BRANCH" ON);
+		cmd+=("A11" "Git delete local $ACTUAl_BRANCH" ON);
+		cmd+=("A13" "Git (final) push to $BASE_BRANCH" ON);
 	fi
-	cmd+=("1b" "Drop release" OFF);
+	cmd+=("A1B" "Drop release" OFF);
+	if [ -x "$(command -v make-springboot-deb)" ]; then
+		cmd+=("A15" "Make deb package" OFF);
+		if [ -x "$(command -v manage-internal-deb-repo)" ]; then
+			cmd+=("A16" "Publish (localy) created deb package" OFF);
+		fi
+	fi
 	ACTION_LIST=$("${cmd[@]}" 3>&1 1>&2 2>&3);
 fi
 
@@ -101,15 +107,15 @@ if [[ "$ACTION_LIST" == ""  ]]; then
 	exit 0;
 fi
 
-if [[ "$ACTION_LIST" =~ "14" ]] ; then
+if [[ "$ACTION_LIST" =~ "A14" ]] ; then
 	edit-changelog;
 fi
 
-if [[ "$ACTION_LIST" =~ "4" ]] ; then
+if [[ "$ACTION_LIST" =~ "A04" ]] ; then
 	mvn clean test -Djacoco.skip=true -Dlicense.skipAddThirdParty=true -Dagent=false
 fi
 
-if [[ "$ACTION_LIST" =~ "9" ]] ; then
+if [[ "$ACTION_LIST" =~ "A09" ]] ; then
 	if [ "$($RELATIVE_DIR/is-gh.bash)" -eq 1 ]; then
 		if [ "$(whiptail --yesno "Do you want create a branch based on GitHub issue ?" 0 0 3>&1 1>&2 2>&3 ; echo $?)" -eq 0 ]; then
 			NEW_BRANCH_NAME=$($RELATIVE_DIR/gh-select-issue.sh);
@@ -138,12 +144,12 @@ if [[ "$ACTION_LIST" =~ "9" ]] ; then
 	git push --set-upstream origin "$NEW_BRANCH_NAME"
 fi
 
-if [[ "$ACTION_LIST" =~ "12" ]] ; then
+if [[ "$ACTION_LIST" =~ "A12" ]] ; then
 	gh pr status
 	gh pr checks
 fi
 
-if [[ "$ACTION_LIST" =~ "14" ]] ; then
+if [[ "$ACTION_LIST" =~ "A14" ]] ; then
 	git add "$CHANGELOGFILE";
 fi
 
@@ -156,20 +162,20 @@ else
 	write-version-file-for-prodlib "$PRODLIB_VERSION_FILE" "$NEW_POM_VERSION"
 fi
 
-if [[ $ACTION_LIST =~ "1c" ]] ; then
+if [[ $ACTION_LIST =~ "A1C" ]] ; then
 	mvn clean deploy -DstagingProgressTimeoutMinutes=30 -Dgpg.skip=false -DskipTests=true -Djacoco.skip=true -Dmaven.test.skip
 fi
-if [[ $ACTION_LIST =~ "1d" ]] ; then
+if [[ $ACTION_LIST =~ "A1D" ]] ; then
         mvn clean deploy -DstagingProgressTimeoutMinutes=30 -Dgpg.skip=false
 fi
-if [[ $ACTION_LIST =~ "6" ]] ; then
+if [[ $ACTION_LIST =~ "A06" ]] ; then
 	mvn clean install -Dgpg.skip=false -DskipTests=true
 	echo "Install locally $NEW_POM_VERSION"
 fi
-if [[ $ACTION_LIST =~ "1a" ]] ; then
+if [[ $ACTION_LIST =~ "A1A" ]] ; then
 	mvn nexus-staging:release -DstagingProgressTimeoutMinutes=30 -Dgpg.skip=false
 fi
-if [[ $ACTION_LIST =~ "1b" ]] ; then
+if [[ $ACTION_LIST =~ "A1B" ]] ; then
 	mvn nexus-staging:drop -DstagingProgressTimeoutMinutes=30 -Dgpg.skip=false
 fi
 
@@ -202,58 +208,71 @@ addDocsReadme () {
 	fi
 }
 
-if [[ $ACTION_LIST =~ "5" ]] ; then
+if [[ $ACTION_LIST =~ "A05" ]] ; then
 	addPOM
 	addTHIRDPARTY
 	addDocsReadme
 	git commit -m "Open version $NEW_POM_VERSION"
 fi
-if [[ $ACTION_LIST =~ "2" ]] ; then
+if [[ $ACTION_LIST =~ "A02" ]] ; then
 	addPOM
 	addTHIRDPARTY
 	addDocsReadme
 	git commit -m "Set version $NEW_POM_VERSION"
 fi
-if [[ $ACTION_LIST =~ "3" ]] ; then
+if [[ $ACTION_LIST =~ "A03" ]] ; then
 	git tag "$NEW_POM_VERSION"
 fi
-if [[ $ACTION_LIST =~ "7" ]] ; then
+if [[ $ACTION_LIST =~ "A07" ]] ; then
 	git push
 fi
-if [[ $ACTION_LIST =~ "8" ]] ; then
+if [[ $ACTION_LIST =~ "A08" ]] ; then
 	git push --tags
 fi
 
 if [[ $IS_MAIN_BRANCH == "0" ]] ; then
-	if [[ $ACTION_LIST =~ "10" ]] ; then
+	if [[ $ACTION_LIST =~ "A10" ]] ; then
 		# Git merge to base branch
 		git co "$BASE_BRANCH"
 		git merge "$ACTUAl_BRANCH"
 		echo "Merge to local $BASE_BRANCH is done. You can now push to distant $BASE_BRANCH and/or rebase before."
-		if [[ $ACTION_LIST =~ "11" ]] ; then
+		if [[ $ACTION_LIST =~ "A11" ]] ; then
 			# Git delete current branch
 			"$RELATIVE_DIR/git-delete-local-branch" "$ACTUAl_BRANCH"
 		fi
-		if [[ $ACTION_LIST =~ "13" ]] ; then
+		if [[ $ACTION_LIST =~ "A13" ]] ; then
 			# Git push to base branch
 			git push
 		fi
 	else
-		if [[ $ACTION_LIST =~ "11" ]] ; then
+		if [[ $ACTION_LIST =~ "A11" ]] ; then
 			echo "Can't delete a non-merged branch ($ACTUAl_BRANCH).";
 		fi
-		if [[ $ACTION_LIST =~ "13" ]] ; then
+		if [[ $ACTION_LIST =~ "A13" ]] ; then
 			echo "Nothing to push";
 		fi
 	fi
 else
-	if [[ $ACTION_LIST =~ "10" ]] ; then
+	if [[ $ACTION_LIST =~ "A10" ]] ; then
 		echo "Can't merge current branch: you're in a current base branch"
 	fi
-	if [[ $ACTION_LIST =~ "11" ]] ; then
+	if [[ $ACTION_LIST =~ "A11" ]] ; then
 		echo "Can't delete current branch: you're in a current base branch"
 	fi
-	if [[ $ACTION_LIST =~ "13" ]] ; then
+	if [[ $ACTION_LIST =~ "A13" ]] ; then
 		echo "Can't push to final branch: you're in a current base branch"
 	fi
+fi
+
+if [[ $ACTION_LIST =~ "A15" ]] ; then
+	make-springboot-deb .
+fi
+
+if [[ $ACTION_LIST =~ "A16" ]] ; then
+	LAST_CREATED_DEB=$(find . -maxdepth 1 -type f -name "*.deb" -printf "%T@ %p\n" | sort -n | tail -1 | cut -d " " -f 2);
+	if [ ! -f "$LAST_CREATED_DEB" ]; then
+		echo "Can't found last created deb file here."
+		exit 1;
+	fi
+	manage-internal-deb-repo "$LAST_CREATED_DEB";
 fi
